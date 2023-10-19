f4328809da2d6812b285273f148210c260374e35
connoromalley <william.omalley@umconnect.umt.edu>
2023-10-19 16:57:05 -0600

    added title and labels

--- mpl_squares.py
+++ mpl_squares.py
@@ -3,7 +3,15 @@ import matplotlib.pyplot as plt
 squares = [1, 4, 9, 16, 25]
 
 fig, ax = plt.subplots()
-ax.plot(squares)
+ax.plot(squares, linewidth=3)
+
+# set chart title and lable axes
+ax.set_title("Square Numbers", fontsize=24)
+ax.set_xlabel("Value", fontsize=14)
+ax.set_ylabel("Square of Value", fontsize=14)
+
+# set size of tick labels.
+ax.tick_params(labelsize=14)
 
 plt.show()
 
