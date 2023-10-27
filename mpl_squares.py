8cf2fc35053a05a6bf72aa6c74a62bf83b0c75c0
connoromalley <william.omalley@umconnect.umt.edu>
2023-10-26 22:10:13 -0600

    added a file, seaborn still doesnt work

--- mpl_squares.py
+++ mpl_squares.py
@@ -1,9 +1,10 @@
 import matplotlib.pyplot as plt
+plt.style.available 
 
 input_values = [1, 2, 3, 4, 5]
 squares = [1, 4, 9, 16, 25]
 
-plt.style.use('seaborn')
+#plt.style.use('seaborn')
 
 fig, ax = plt.subplots()
 ax.plot(input_values, squares, linewidth=3)
