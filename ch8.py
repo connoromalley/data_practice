b86b1e10fb9927d30c87c062d3776ebcef923f00
connoromalley <william.omalley@umconnect.umt.edu>
2023-11-03 17:08:19 -0600

    update

--- ch8.py
+++ ch8.py
@@ -8,3 +8,16 @@ def make_pizza(size, *toppings):
         print(f"-{topping}")
 
 make_pizza(16, 'pepperoni')
+
+make_pizza(12, 'mushrooms', 'green peppers')
+
+def build_profile(first, last, **user_info):
+    user_info['first_name'] = first
+    user_info['last_name'] = last
+    return user_info
+
+user_profile = build_profile('albert', 'einstein',
+                             location = 'princeton',
+                             field = 'physics')
+
+print(user_profile)
