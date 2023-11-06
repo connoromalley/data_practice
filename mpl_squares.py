a650bbbaa41ebb6a549d9e98bb73694757d3e421
connoromalley <william.omalley@umconnect.umt.edu>
2023-11-05 20:19:54 -0700

    update

--- mpl_squares.py
+++ mpl_squares.py
@@ -1,17 +1,22 @@
 import matplotlib.pyplot as plt
 
-input_values = [1, 2, 3, 4, 5]
-squares = [1, 4, 9, 16, 25]
+x_values = range(1, 1001)
+y_values = [x**2 for x in x_values]
 
-#plt.style.use('seaborn')
+#plt.style.use('seaborn') why doesnt this work??
 
 fig, ax = plt.subplots()
-ax.plot(input_values, squares, linewidth=3)
+ax.scatter(x_values, y_values, s=10)
+#ax.plot(input_values, squares, linewidth=3)
 
 # set chart title and lable axes
 ax.set_title("Square Numbers", fontsize=24)
 ax.set_xlabel("Value", fontsize=14)
 ax.set_ylabel("Square of Value", fontsize=14)
+
+
+# set the range for each axis.
+ax.axis([0, 1100, 0, 1_100_000])
 
 # set size of tick labels.
 ax.tick_params(labelsize=14)
