7ba2df546dd8ef0fe0175dfca6821fdd2a049f00
connoromalley <william.omalley@umconnect.umt.edu>
2023-11-05 20:48:32 -0700

    added random walk file

--- mpl_squares.py
+++ mpl_squares.py
@@ -6,7 +6,9 @@ y_values = [x**2 for x in x_values]
 #plt.style.use('seaborn') why doesnt this work??
 
 fig, ax = plt.subplots()
-ax.scatter(x_values, y_values, s=10)
+#ax.scatter(x_values, y_values, color='green', s=10)
+#ax.scatter(x_values, y_values, color=(0,0.3, .3), s=10)
+ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues, s=10)
 #ax.plot(input_values, squares, linewidth=3)
 
 # set chart title and lable axes
@@ -17,9 +19,15 @@ ax.set_ylabel("Square of Value", fontsize=14)
 
 # set the range for each axis.
 ax.axis([0, 1100, 0, 1_100_000])
+ax.ticklabel_format(style='plain') # this allows you to override default y axis
+
 
 # set size of tick labels.
 ax.tick_params(labelsize=14)
 
 plt.show()
+
+
+#save plot with this command:
+plt.savefig('squares_plot.png', bbox_inches='tight')
 
