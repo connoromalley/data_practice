6cdf99af6dafc50694c0a2cd063776eab5513bbc
connoromalley <william.omalley@umconnect.umt.edu>
2023-11-27 21:43:15 -0700

    updated weather analysis

--- sitka_highs.py
+++ sitka_highs.py
@@ -2,27 +2,41 @@ from pathlib import Path
 import csv
 
 import matplotlib.pyplot as plt
+from datetime import datetime
 
-path = Path('weather_data/sitka_weather_2021_simple.csv')
+path = Path('weather_data/death_valley_2021_simple.csv')
+#path = Path('weather_data/sitka_weather_2021_simple.csv')
 lines = path.read_text().splitlines()
 
 reader = csv.reader(lines)
 header_row = next(reader)
 
-# extract high temperatures
-highs = []
+# Extract dates and high temperatures
+dates, highs, lows = [], [], []
 for row in reader:
-    high = int(row[4])
-    highs.append(high)
+    current_date = datetime.strptime(row[2], '%Y-%m-%d')
+    try:
+        high = int(row[4])
+        low = int(row[5])
+    except ValueError:
+        print(f"Missing data for {current_date}")
+    else:
+        dates.append(current_date)
+        highs.append(high)
+        lows.append(low)
 
-# plot the high temperatures
+# plot the high and low temperatures
 plt.style.use('classic')
 fig, ax = plt.subplots()
-ax.plot(highs, color='red')
+ax.plot(dates, highs, color='red', alpha=0.5)
+ax.plot(dates, lows, color='red', alpha=0.5)
+ax.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
+
 
 # format plot
-ax.set_title("Daily high temperatures, July 2021", fontsize = 24)
+ax.set_title("Daily high and low temperatures, July 2021\nDeath Valley, CA", fontsize = 24)
 ax.set_xlabel('', fontsize=16)
+fig.autofmt_xdate() 
 ax.set_ylabel("Temperature (F)", fontsize=16)
 ax.tick_params(labelsize=16)
 
