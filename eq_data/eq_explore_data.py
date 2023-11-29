40dc87cabb7cab3d3ec0064ab29dcc655be2b818
connoromalley <william.omalley@umconnect.umt.edu>
2023-11-29 16:55:54 -0700

    finished earthquake world map

--- eq_data/eq_explore_data.py
+++ eq_data/eq_explore_data.py
@@ -9,9 +9,15 @@ all_eq_data = json.loads(contents)
 #examine all earthquakes in the dataset
 all_eq_dicts = all_eq_data['features']
 
-mags = []
+mags, lons, lats = [], [], []
 for eq_dict in all_eq_dicts:
     mag = eq_dict['properties']['mag']
+    lon = eq_dict['geometry']['coordinates'][0]
+    lat = eq_dict['geometry']['coordinates'][0]
     mags.append(mag)
+    lons.append(mag)
+    lats.append(lat)
 
 print(mags[:10])
+print(lons[:5])
+print(lats[:5])
