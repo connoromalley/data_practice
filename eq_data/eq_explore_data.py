c0458ebbd8100f388be693c4a6920e9d2c862331
connoromalley <william.omalley@umconnect.umt.edu>
2023-11-29 16:20:50 -0700

    earthquake file update

--- eq_data/eq_explore_data.py
+++ eq_data/eq_explore_data.py
@@ -6,7 +6,12 @@ path = Path('eq_data/eq_data_1_day_m1.geojson')
 contents = path.read_text()
 all_eq_data = json.loads(contents)
 
-#create a more readable version of the data file
-path = Path('eq_data/readable_eq_data.geojson')
-readable_contents = json.dumps(all_eq_data, indent=4)
-path.write_text(readable_contents)
+#examine all earthquakes in the dataset
+all_eq_dicts = all_eq_data['features']
+
+mags = []
+for eq_dict in all_eq_dicts:
+    mag = eq_dict['properties']['mag']
+    mags.append(mag)
+
+print(mags[:10])
